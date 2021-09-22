cfab576737473ebd00d0f1c6e03a8fd0d06ec8ce
oscar-quiroz <kairox.rt@gmail.com>
2021-09-22 11:51:55 -0500

    crear usuario y obtener usuarios.. encriptar contraseña de creacion de usuario

--- controladores/usuario.controller.js
+++ controladores/usuario.controller.js
@@ -1,24 +1,47 @@
-const Usuario = require('../models/usuario');
+const Usuario = require("../models/usuario");
+const { response } = require("express");
+const bcrypt = require("bcryptjs");
 
-const getUsuarios = (req, res) => {
+//GET
+const getUsuarios = async(req, res) => {
+    const usuario = await Usuario.find({}, "nombre email role google");
     res.json({
         ok: true,
-        usuarios: [],
+        usuario,
     });
 };
 
-const crearUsuario = async(req, res) => {
-    console.log(req.body);
-    const { email, password, nombre } = req.body;
+//POST
+const crearUsuario = async(req, res = response) => {
+    const { email, password } = req.body;
 
-    const usuario = new Usuario(req.body);
+    try {
+        const existeEmail = await Usuario.findOne({ email });
+        if (existeEmail) {
+            return res.status(400).json({
+                ok: false,
+                msg: "correo duplicado",
+            });
+        }
 
-    await usuario.save();
+        const usuario = new Usuario(req.body);
 
-    res.json({
-        ok: true,
-        usuario
-    });
+        //encriptar la contraseña
+        const salt = bcrypt.genSaltSync();
+        usuario.password = bcrypt.hashSync(password, salt);
+
+        await usuario.save();
+        res.json({
+            ok: true,
+            usuario,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            ok: false,
+            msg: "error inesperado",
+        });
+    }
 };
 
 module.exports = {
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -4,13 +4,22 @@
 const { Router } = require("express");
 const {
     getUsuarios,
-    crearUsuario,
+    crearUsuario
 } = require("../controladores/usuario.controller");
+const { check } = require("express-validator");
+const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
 router.get("/", getUsuarios);
 
-router.post("/", crearUsuario);
+router.post(
+    "/", [
+        check("nombre", "El nombre es obligatorio").not().isEmpty(),
+        check("password", "La constraseña es obligatoria").not().isEmpty(),
+        check("email", "El correo es obligatorio").isEmail(),
+        validarCampos
+    ], crearUsuario
+);
 
 module.exports = router;
