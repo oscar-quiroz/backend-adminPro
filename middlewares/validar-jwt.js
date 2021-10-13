afd9cede19a5aedaf623c39faed904c99fcc8fd5
oscar-quiroz <kairox.rt@gmail.com>
2021-10-13 18:57:15 -0500

    token en los headers de las peticiones

---
+++ middlewares/validar-jwt.js
@@ -0,0 +1,38 @@
+const jwt = require('jsonwebtoken');
+const validarJwt = (req, res, next) => {
+
+    //leer headers
+
+    const token = req.header('x-token')
+
+
+    if (!token) {
+        return res.status(401).json({
+            ok: false,
+            msg: "no hay token en la peticion"
+        })
+    }
+
+
+    try {
+
+        const { uid } = jwt.verify(token, process.env.JWT_SECRET)
+
+        req.uid = uid
+
+        next();
+
+    } catch (error) {
+        return res.status(401).json({
+            ok: fasle,
+            msg: 'token no valido'
+        })
+    }
+
+
+
+}
+
+module.exports = {
+    validarJwt
+}
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -6,33 +6,39 @@ const {
     getUsuarios,
     crearUsuario,
     actualizarUsuario,
-    eliminarUsuario
+    eliminarUsuario,
 } = require("../controladores/usuario.controller");
+
 const { check } = require("express-validator");
-const { validarCampos } = require('../middlewares/validar-campos');
+const { validarCampos } = require("../middlewares/validar-campos");
+const { validarJwt } = require("../middlewares/validar-jwt");
 
 const router = Router();
 
-router.get("/", getUsuarios);
+router.get("/", [validarJwt], getUsuarios);
 
 router.post(
     "/", [
+
         check("nombre", "El nombre es obligatorio").not().isEmpty(),
         check("password", "La constraseña es obligatoria").not().isEmpty(),
         check("email", "El correo es obligatorio").isEmail(),
-        validarCampos
-    ], crearUsuario
+        validarCampos,
+    ],
+    crearUsuario
 );
 
-router.put("/:id", [
-    check("nombre", "El nombre es obligatorio").not().isEmpty(),
-    check("role", "El rol es obligatorio").not().isEmpty(),
-    check("email", "El correo es obligatorio").isEmail(),
-    validarCampos
-], actualizarUsuario);
+router.put(
+    "/:id", [
+        validarJwt,
+        check("nombre", "El nombre es obligatorio").not().isEmpty(),
+        check("role", "El rol es obligatorio").not().isEmpty(),
+        check("email", "El correo es obligatorio").isEmail(),
+        validarCampos,
+    ],
+    actualizarUsuario
+);
 
-router.delete("/:id", eliminarUsuario);
-
-
+router.delete("/:id", [validarJwt], eliminarUsuario);
 
 module.exports = router;
