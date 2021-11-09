27307ec18fb186bdb5d09bdc19a3246efdfb7b2d
oscar-quiroz <kairox.rt@gmail.com>
2021-11-09 15:01:29 -0500

    respuesta 200 en vez de 400

--- controladores/busquedas.controllers.js
+++ controladores/busquedas.controllers.js
@@ -17,7 +17,11 @@ const getTodo = async(req, res = response) => {
     // const medico = await Medico.find({ nombre: regex });
     // const hospital = await Hospital.find({ nombre: regex });
 
-    const [usuario, medico, hospital] = await Promise.all([Usuario.find({ nombre: regex }), Medico.find({ nombre: regex }), Hospital.find({ nombre: regex })])
+    const [usuario, medico, hospital] = await Promise.all([
+        Usuario.find({ nombre: regex }),
+        Medico.find({ nombre: regex }),
+        Hospital.find({ nombre: regex }),
+    ]);
 
     res.json({
         ok: true,
@@ -29,41 +33,40 @@ const getTodo = async(req, res = response) => {
     });
 };
 
-
 const getDocumentosColeccion = async(req, res = response) => {
-
     const tabla = req.params.tabla;
     const busqueda = req.params.busqueda;
 
     const regex = new RegExp(busqueda, "i");
 
     switch (tabla) {
-        case 'medicos':
-            const medico = await Medico.find({ nombre: regex }).populate('usuario', 'nombre img').populate('hospital', 'nombre img');
-            res.status(400).json({ ok: true, resultado: medico })
+        case "medicos":
+            const medico = await Medico.find({ nombre: regex })
+                .populate("usuario", "nombre img")
+                .populate("hospital", "nombre img");
+            res.json({ ok: true, resultado: medico });
             break;
-        case 'hospitales':
-            const hospital = await Hospital.find({ nombre: regex }).populate('usuario', 'nombre img');
-            res.status(400).json({ ok: true, resultado: hospital })
+        case "hospitales":
+            const hospital = await Hospital.find({ nombre: regex }).populate(
+                "usuario",
+                "nombre img"
+            );
+            res.json({ ok: true, resultado: hospital });
 
             break;
-        case 'usuarios':
-
+        case "usuarios":
             const usuario = await Usuario.find({ nombre: regex });
-            res.status(400).json({ ok: true, resultado: usuario })
+            res.json({ ok: true, resultado: usuario });
 
             break;
         default:
-
-            return res.status(400).json({ ok: false, msg: "solo medicos, hospitales o usuarios" })
-
-
+            return res
+                .status(400)
+                .json({ ok: false, msg: "solo medicos, hospitales o usuarios" });
     }
-
-
 };
 
 module.exports = {
     getTodo,
-    getDocumentosColeccion
+    getDocumentosColeccion,
 };
